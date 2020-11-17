23bdfaec141ec0f53cc14354e3cc84684b65a304
lowie v.e <64786202+RealLowie@users.noreply.github.com>
2020-11-17 12:43:11 +0100

    Add files via upload

---
+++ do not click.bat
@@ -0,0 +1,9 @@
+@echo off
+color a
+echo Welcome to: The Virus.
+echo Your computer is legit dead now. Request backup in 60 sec. If you don't do that, you will lose all your files.
+echo NOT MY PROBLEM, By the way don't restart your pc after this, you will lose everything, including your mind.
+timeout 60
+shutdown -s
+echo This is 100% real. Thanks for making this virus: L0:3E
+pause Press any key to kill your pc.
